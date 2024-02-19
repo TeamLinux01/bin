a34b8c8fa857e108240b60bcde520c32fef67c3b
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2024-02-18 19:33:10 -0500

    Removed obsolete commands

--- create_ssh_key_for_service.sh
+++ create_ssh_key_for_service.sh
@@ -7,7 +7,7 @@ if [[ $# -ne 2 ]] ; then #display an error if both arguments are not entered
     exit 1;
 fi
 
-ssh-keygen -t rsa -b 4096 -C "Server: $1 Username: $2" -f ~/.ssh/$2@$1 &&
+ssh-keygen -t ed25519 -C "Server: $1 Username: $2" -f ~/.ssh/$2@$1 &&
 printf "Created ~/.ssh/$2@$1\nNow adding information to ~/.ssh/config\n" &&
 sed -i '$a Host '$1'\n    HostName '$1'\n    User '$2'\n    IdentityFile ~/.ssh/'$2'@'$1 ~/.ssh/config &&
 printf "Added to ~/.ssh/config:\nHost $1\n    HostName $1\n    User $2\n    IdentityFile ~/.ssh/$2@$1\n";
