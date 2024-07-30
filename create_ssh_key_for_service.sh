e01a0b4aeb04ead08154eaa3342fa8a56d8f94fd
TeamLinux01 <43735175+TeamLinux01@users.noreply.github.com>
2024-07-30 12:10:28 -0400

    Used shellcheck to find issues;fixed the issues

--- create_ssh_key_for_service.sh
+++ create_ssh_key_for_service.sh
@@ -3,11 +3,11 @@
 #argument 1 ($1) server-hostname, argument 2 ($2) username
 
 if [[ $# -ne 2 ]] ; then #display an error if both arguments are not entered
-    printf "Missing arguments: server-hostname, username\nExample:\n\n$0 server-hostname username\n";
+    printf "Missing arguments: server-hostname, username\nExample:\n%s server-hostname username\n" "$0";
     exit 1;
 fi
 
-ssh-keygen -t ed25519 -C "Server: $1 Username: $2" -f ~/.ssh/$2@$1 &&
-printf "Created ~/.ssh/$2@$1\nNow adding information to ~/.ssh/config\n" &&
-sed -i '$a Host '$1'\n    HostName '$1'\n    User '$2'\n    IdentityFile ~/.ssh/'$2'@'$1 ~/.ssh/config &&
-printf "Added to ~/.ssh/config:\nHost $1\n    HostName $1\n    User $2\n    IdentityFile ~/.ssh/$2@$1\n";
+ssh-keygen -t ed25519 -C "Server: $1 Username: $2" -f ~/.ssh/"$2"@"$1" &&
+printf "Created ~/.ssh/%s@%s\nNow adding information to ~/.ssh/config\n" "$2" "$1" &&
+sed -i '$a Host '"$1"'\n    HostName '"$1"'\n    User '"$2"'\n    IdentityFile ~/.ssh/'"$2"'@'"$1" ~/.ssh/config &&
+printf "Added to ~/.ssh/config:\nHost %s\n    HostName %s\n    User %s\n    IdentityFile ~/.ssh/%s@%s\n" "$1" "$1" "$2" "$2" "$1";
